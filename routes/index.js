acd682c666079ee16c1e2c67fea8c5b152dec5f5
Anthony Simpson <ansi6622@colorado.edu>
2016-10-19 13:35:40 -0700

    small edits

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Javascript kNN Exercise' });
+});
+router.get('/img', function(req, res, next) {
+  res.render('img', { title: 'Javascript kNN Exercise with an image!!!!' });
 });
 
 module.exports = router;
